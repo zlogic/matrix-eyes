cd99e12b1d3c2a7dc5c83ff93f3ac86faa510804
Dmitry Zolotukhin <zlogic@gmail.com>
2023-01-21 19:09:46 +0000

    Added option to output OBJ mesh.
    Also, allow to scale image (e.g. to keep mesh size reasonable)

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ To use a custom Torch home directory, set the `TORCH_HOME` environment variable.
 To gerate a depth image, run:
 
 ```shell
-python main.py [--model-type=DPT_Large|DPT_Hybrid|MiDaS_small] <input file> <output_file>
+python main.py [--model-type=DPT_Large|DPT_Hybrid|MiDaS_small] [--output-format=image|mesh] <input file> <output_file>
 ```
 
 replacing `<input file>` with the source image filename, and `<output file>` with the output destination filename.
@@ -27,6 +27,9 @@ replacing `<input file>` with the source image filename, and `<output file>` wit
 Additional (optional) arguments:
 
 * `model-type` specifies one of the MiDaS models
-    * `DPT_Large` will use MiDaS v3 - Large (highest accuracy, slowest inference speed)
+    * `DPT_Large` will use MiDaS v3 - Large (highest accuracy, slowest inference speed); default
     * `DPT_Hybrid` will use MiDaS v3 - Hybrid (medium accuracy, medium inference speed)
     * `MiDaS_small` will use MiDaS v2.1 - Small (lowest accuracy, highest inference speed)
+* `output-format` specifies what to output
+    * `image` will output a depth map image
+    * `mesh` will output a 3D [Wavefront OBJ file](https://en.wikipedia.org/wiki/Wavefront_.obj_file)
--- main.py
+++ main.py
@@ -41,6 +41,19 @@ class MiDaS:
         self.model_type = model_type
         self.load_model()
 
+def output_mesh(depth, filename):
+    width = depth.shape[1]
+    height = depth.shape[0]
+    with open(filename, 'w') as f:
+        for y in range(height):
+            for x in range(width):
+                f.write(f'v {x} {height-y} {depth[y][x]}\n')
+
+        for y in range(height-1):
+            for x in range(width-1):
+                f.write(f'f {y*width+x+1} {(y+1)*width+x+1} {y*width+(x+1)+1}\n')
+                f.write(f'f {y*width+(x+1)+1} {(y+1)*width+x+1} {(y+1)*width+(x+1)+1}\n')
+
 def output_image(depth, filename):
     depth_min = depth.min()
     depth_max = depth.max()
@@ -55,18 +68,27 @@ def main():
     parser.add_argument('--model-type',
         choices=['DPT_Large', 'DPT_Hybrid', 'MiDaS_small'],
         default='DPT_Large')
+    parser.add_argument('--output-format', choices=['image', 'mesh'], default='image')
+    parser.add_argument('--resize-scale', type=float, default=1.0)
     parser.add_argument('input')
     parser.add_argument('output')
     args = parser.parse_args()
 
     img = cv2.imread(args.input)
+
+    if args.resize_scale != 1.0:
+        scale = args.resize_scale
+        img = cv2.resize(img, (int(img.shape[1]*scale), int(img.shape[0]*scale)), interpolation=cv2.INTER_CUBIC)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     midas = MiDaS(args.model_type)
     output = midas.extract_depth(img)
 
     del(midas)
-    output_image(output, args.output)
+    if args.output_format == 'image':
+        output_image(output, args.output)
+    elif args.output_format == 'mesh':
+        output_mesh(output, args.output)
 
 if __name__ == '__main__':
     main()
