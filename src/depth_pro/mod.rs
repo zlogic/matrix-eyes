f30aafabfd7fc4776044094661f428ae273cdfb4
Dmitry Zolotukhin <zlogic@gmail.com>
2025-01-08 23:07:54 +0100

    Added a decoder based on Candle DepthAnythingV2.
    At least the dimensions match, although memory usage is almost 10GB.

--- src/depth_pro/mod.rs
+++ src/depth_pro/mod.rs
@@ -1,17 +1,28 @@
-use candle_nn::{Module as _, VarBuilder};
+use candle_nn::VarBuilder;
+use decoder::MultiresConvDecoder;
 use encoder::DepthProEncoder;
 
+mod decoder;
 mod encoder;
 
 pub struct DepthPro {
     encoder: DepthProEncoder,
+    decoder: MultiresConvDecoder,
 }
 
 impl DepthPro {
     pub fn new(vb: VarBuilder) -> Result<DepthPro, candle_core::Error> {
-        let encoder = DepthProEncoder::new(vb.pp("encoder"))?;
+        const ENCODER_FEATURE_DIMS: [usize; 4] = [256, 512, 1024, 1024];
+        const DECODER_FEATURES: usize = 256;
 
-        Ok(DepthPro { encoder })
+        let encoder =
+            DepthProEncoder::new(vb.pp("encoder"), &ENCODER_FEATURE_DIMS, DECODER_FEATURES)?;
+
+        let mut dims_encoder = vec![DECODER_FEATURES];
+        dims_encoder.extend_from_slice(&ENCODER_FEATURE_DIMS);
+        let decoder = MultiresConvDecoder::new(vb.pp("decoder"), &dims_encoder, DECODER_FEATURES)?;
+
+        Ok(DepthPro { encoder, decoder })
     }
 
     pub fn extract_depth(&self, img: &candle_core::Tensor) -> Result<(), candle_core::Error> {
@@ -24,6 +35,8 @@ impl DepthPro {
             encodings[3].dims(),
             encodings[4].dims()
         );
+        let (features, features_0) = self.decoder.forward(encodings)?;
+        println!("Decoded {:?} {:?}", features.dims(), features_0.dims());
         Ok(())
     }
 }
