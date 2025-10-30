aae68af418064cf212699e1a7a78c5fa83309593
Dmitry Zolotukhin <zlogic@gmail.com>
2025-10-30 20:53:30 +0100

    Use subslices for wgpu memory.
    This seems to fix results on Windows wgpu.

--- src/reconstruction.rs
+++ src/reconstruction.rs
@@ -57,7 +57,7 @@ pub fn init_device() -> burn::backend::wgpu::WgpuDevice {
     let device = burn::backend::wgpu::WgpuDevice::DefaultDevice;
     let runtime_options = burn::backend::wgpu::RuntimeOptions {
         tasks_max: 1,
-        memory_config: burn::backend::wgpu::MemoryConfiguration::ExclusivePages,
+        memory_config: burn::backend::wgpu::MemoryConfiguration::SubSlices,
     };
     burn::backend::wgpu::init_setup::<WgpuApi>(&device, runtime_options);
     device
